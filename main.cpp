5fbb48bc4737bb02ec86719cc76130da456b5fc7
stefanbalta <stefan.balta.sb@gmail.com>
2025-02-16 20:56:09 -0500

    Added window and surface creation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 class Game {
 private:
+    GLFWwindow* m_window;
     vk::Instance m_instance;
     vk::PhysicalDevice m_physicalDevice;
     vk::Device m_device;
@@ -14,11 +15,17 @@ private:
     vk::Format m_swapChainImageFormat;
     vk::Extent2D m_swapChainExtent;
     std::vector<vk::ImageView> m_swapChainImageViews;
+    void createWindow(){
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        m_window = glfwCreateWindow(800, 600, "Vulkan Block Game", nullptr, nullptr);
+    }
     void createInstance() {
         vk::ApplicationInfo appInfo("Vulkan Block Game", VK_MAKE_VERSION(1, 0, 0), "No Engine", VK_MAKE_VERSION(1, 0, 0), VK_API_VERSION_1_2);
         vk::InstanceCreateInfo createInfo;
         createInfo.pApplicationInfo = &appInfo;
-        std::vector<const char*> extensions = {VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME};
+        std::vector<const char*> extensions = {VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME, VK_KHR_SURFACE_EXTENSION_NAME};
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
@@ -80,15 +87,27 @@ private:
         m_device = m_physicalDevice.createDevice(createInfo);
         m_queue = m_device.getQueue(0, 0);
     }
+    void createSurface(){
+        VkSurfaceKHR surface;
+        if(glfwCreateWindowSurface(m_instance, m_window, nullptr, &surface) != VK_SUCCESS){
+            throw std::runtime_error("Failed to create window surface");
+        }
+        m_surface = surface;
+    }
     void cleanupResources() {
+        m_instance.destroySurfaceKHR(m_surface);
         m_device.destroy();
         m_instance.destroy();
+        glfwDestroyWindow(m_window);
+        glfwTerminate();
     }
 public:
     Game(){
+        createWindow();
         createInstance();
         pickPhysicalDevice();
         createLogicalDevice();
+        createSurface();
     }
     ~Game(){
         cleanupResources();
@@ -100,7 +119,7 @@ public:
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     Game game;
+    game.run();
     return 0;
 }
